7235c0ecf0bc6d04c662bbcae389f71103e305bd
lunanie <lunanie@gmail.com>
2021-04-18 19:16:19 -0300

    header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  toolbar: {
+    minHeight: 128,
+    alignItems: 'flex-start',
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    alignSelf: 'flex-end',
+  },
+}));
+
+export const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h1" component="h1" >
+            Luciane Nagay Martins
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
